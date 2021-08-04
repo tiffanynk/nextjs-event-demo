195611767eb24b4504aa59642b93d6bcbafda36c
Tiffany Kanjanabout <tiffany.kanjanabout@flatironschool.com>
2021-08-04 16:13:19 -0600

    adds individual event components and styles

---
+++ components/EventDetails/EventLogistics.js
@@ -0,0 +1,34 @@
+import AddressIcon from '../ui/icons/AddressIcon';
+import DateIcon from '../ui/icons/DateIcon';
+import LogisticsItem from './LogisticsItem';
+import classes from './EventLogistics.module.css';
+
+function EventLogistics(props) {
+  const { date, address, image, imageAlt } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const addressText = address.replace(', ', '\n');
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={`/${image}`} alt={imageAlt} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={DateIcon}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={AddressIcon}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  );
+}
+
+export default EventLogistics;
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,31 @@
+import { useRouter } from "next/dist/client/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/EventDetails/EventSummary";
+import EventLogistics from "../../components/EventDetails/EventLogistics";
+import EventContent from "../../components/EventDetails/EventContent";
+
 export const DetailedEvent = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId)
+
+  if(!event) {
+    return <p>No event found!</p>
+  }
+
   return (
-    <div>
-      <h1>A Single Event</h1>
-    </div>
+    <>
+      <EventSummary title={event.title}/>
+      <EventLogistics
+        address={event.location}
+        date={event.date}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        {event.description}
+      </EventContent>
+    </>
   );
 }
 
